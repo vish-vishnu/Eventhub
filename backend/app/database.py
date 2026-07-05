e41fbda573189f306e2576366353e094409e1111
vish-vishnu <vishnua2523@gmail.com>
2026-07-05 22:40:15 +0530

    Make database connection dynamic

--- backend/app/database.py
+++ backend/app/database.py
@@ -1,8 +1,9 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
 #getting postgresql credentials
-SQLALCHEMY_DATABASE_URL="postgresql://postgres:Vishnu2523@localhost:5432/event_booking_db"
+SQLALCHEMY_DATABASE_URL=os.getenv("DATABASE_URL","postgresql://postgres:Vishnu2523@localhost:5432/event_booking_db")
 
 #connect with database
 engine=create_engine(SQLALCHEMY_DATABASE_URL)
